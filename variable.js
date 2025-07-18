3303efbbd62e19a35c0f31a671fabd04f98f3436
prokaiteam <prokai.team@gmail.com>
2025-07-18 13:09:46 +0530

    Delete variable.js

--- variable.js
+++
@@ -1,7 +0,0 @@
-import { TITLE,SUBTITLE } from "./js/constants.js";
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("constant-title").innerText = TITLE;
-  document.getElementById("constant-subtitle").innerHTML = SUBTITLE;
-});
